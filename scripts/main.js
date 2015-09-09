60c349d977ab69f2df6e1b74dca1051758aa08b4
WebRainCoder <webraincoder@gmail.com>
2015-09-10 00:55:23 +0300

    rocket throw the icon

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,5 @@
 (function() {
-  var animElements, gravitation;
 
-  animElements = [];
-
-  animElements.push(document.querySelector('#rocket-wrap'), document.querySelector('#plate-wrap'));
-
-  gravitation = function(el) {
-    return el.style.animation = "gravitation .8s linear infinite";
-  };
 
 }).call(this);
 
